d486437641fb985dd38da3618ad776cf0062dab4
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-27 20:37:30 -0400

    Fetch and show product and categort count on admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV["USERNAME"], :password => ENV["PASSWORD"]
 
   def show
+    @product_count = Product.all.count
+    @category_count = Category.all.count
   end
   
 end
