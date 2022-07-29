207c5d07a4f84bc167fd2fd843216aa5fd0469a6
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-28 22:15:26 -0400

    Fix error related to undefined variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to '/'
     else
       redirect_to '/signup'
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(
       :first_name,
