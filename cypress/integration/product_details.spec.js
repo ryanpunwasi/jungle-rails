c6fd9cda02de7ef151b108a8ab11e20271bf78b9
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-02 15:56:58 -0400

    Add integration test to check if individual product page is shown when product is clicked on home page

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,10 @@
+describe("Jungle Products Page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("shows an individual product when article element is clicked on", () => {
+    cy.get(".products article").first().click();
+    cy.get("article h1").should("have.text", "Scented Blade");
+  });
+});
