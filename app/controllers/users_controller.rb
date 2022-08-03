f0a4a408b4f1f80011a96e05efaa26509d5922c9
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-08-02 21:58:57 -0400

    Fix error related to chained bang operations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    @user.email.downcase!.strip!
+    @user.email.downcase!
+    @user.email.strip!
 
     if @user.save
       session[:user_id] = @user.id
